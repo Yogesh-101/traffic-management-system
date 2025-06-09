785808b1185fedefa6d6d3b03da9dc9cc1670585
Yogesh-101 <yogeshmacharla19@gmail.com>
2025-06-09 14:06:04 +0530

    Fix copy-redirects.js import syntax error

--- scripts/copy-redirects.js
+++ scripts/copy-redirects.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
-import { fileURLToPath, dirname } from 'url';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
