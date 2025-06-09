0b3f701932c66e21d89e5d8899e473182d27e819
Yogesh-101 <yogeshmacharla19@gmail.com>
2025-06-09 13:54:49 +0530

    Update copy-redirects.js to use ES modules

--- scripts/copy-redirects.js
+++ scripts/copy-redirects.js
@@ -1,11 +1,19 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath, dirname } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const redirectsPath = path.join(__dirname, '../_redirects');
 const distPath = path.join(__dirname, '../dist/_redirects');
 
-if (fs.existsSync(redirectsPath)) {
-    fs.copyFileSync(redirectsPath, distPath);
-} else {
-    console.log('No _redirects file found');
+try {
+    if (await fs.promises.access(redirectsPath)) {
+        await fs.promises.copyFile(redirectsPath, distPath);
+    } else {
+        console.log('No _redirects file found');
+    }
+} catch (error) {
+    console.error('Error copying redirects:', error);
 }
